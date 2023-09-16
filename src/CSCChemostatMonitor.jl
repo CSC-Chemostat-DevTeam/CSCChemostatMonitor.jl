e5371e42fa1ceef975259f8d3b283f44bfbb4849
josePereiro <jose.pereiro.personal@gmail.com>
2023-09-16 00:45:45 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.44

---
+++ src/CSCChemostatMonitor.jl
@@ -0,0 +1,5 @@
+module CSCChemostatMonitor
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CSCChemostatMonitor
+using Test
+
+@testset "CSCChemostatMonitor.jl" begin
+    # Write your tests here.
+end
